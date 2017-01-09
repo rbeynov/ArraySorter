18a747b0c799ae1352ccb13fbe46c868945e69f7
rbeneteau <r.beneteau@outlook.fr>
2017-01-10 00:49:08 +0100

    Add bubble sorting capability

--- src/main/java/com/ynov/arraysorter/ArraySorter.java
+++ src/main/java/com/ynov/arraysorter/ArraySorter.java
@@ -3,9 +3,40 @@ package com.ynov.arraysorter;
 public class ArraySorter {
 
     public static void main(String[] args) {
+        // Initialisation de 2 tableaux d'entier
         int[] toSortArray = { 43, 80, 81, 49, 83, 20, 97, 82, 43, 40 };
+        int[] sortedArray;
 
+        // Tri bulle
+        System.out.println("---- Bubble Sort ----");
         displayArray(toSortArray);
+        sortedArray = bubbleSort(toSortArray);
+        displayArray(sortedArray);
+    }
+
+    /**
+     * Tri un tableau d'entier en ordre croissant via l'algo bulle.
+     *
+     * @param array
+     *            Tableau à trier.
+     * @return Tableau trié.
+     */
+    static int[] bubbleSort(int[] array) {
+        int size = array.length;
+
+        for (int round = 1; round < size; round++) { // Compte le nombre de tour
+            // La boucle interne devient de plus en plus petite
+            for (int i = 0; i < size - round; i++) {
+                if (array[i] > array[i+1]) {
+                    // Echange dans le tableau final
+                    int temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                }
+            }
+        }
+
+        return array;
     }
 
     /**
