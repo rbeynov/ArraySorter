00e663093b6586f7ba2dc0b7a6a2b2681620a818
rbeneteau <r.beneteau@outlook.fr>
2017-01-10 00:43:56 +0100

    Add table of number and business to display content of the table

--- src/main/java/com/ynov/arraysorter/ArraySorter.java
+++ src/main/java/com/ynov/arraysorter/ArraySorter.java
@@ -3,7 +3,26 @@ package com.ynov.arraysorter;
 public class ArraySorter {
 
     public static void main(String[] args) {
-        // Empty entry point
+        int[] toSortArray = { 43, 80, 81, 49, 83, 20, 97, 82, 43, 40 };
+
+        displayArray(toSortArray);
+    }
+
+    /**
+     * Affiche les valeurs d'un tableau.
+     * @param array
+     *            Tableau dont on veut afficher le contenu.
+     */
+    static void displayArray(int[] array) {
+        // Itération de tableau
+        // Pour chaque int présent dans "array"
+        for (int i = 0; i < array.length; i++) {
+            int numberToDisplay = array[i];
+            System.out.print(numberToDisplay+" ");
+        }
+
+        // Saut de ligne
+        System.out.print("\n"); // ou System.out.println("");
     }
 
 }
